1e8db4e795a4a144c6f97af97c4faef3b89644f5
Mehmet Bertan Tarakçıoğlu <83073748+BertanT@users.noreply.github.com>
2022-10-08 00:57:45 +0300

    Create Perticle Firmware Project and add a little bit of code, add more documents to the planning folder

---
+++ ZG_Particle_Firmware/lib/DFRobot_BME280/examples/raedDataI2C/raedDataI2C.ino
@@ -0,0 +1,65 @@
+/*!
+ * @file  raedDataI2C.ino
+ * @brief  Download this demo to test read data from bme280, connect sensor through
+ * @n  IIC interface. Data will print on your serial monitor
+ * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @license  The MIT License (MIT)
+ * @author  [Frank](jiehan.guo@dfrobot.com)
+ * @maintainer  [qsjhyy](yihuan.huang@dfrobot.com)
+ * @version  V1.0
+ * @date  2022-06-16
+ * @url  https://github.com/DFRobot/DFRobot_BME280
+ */
+#include "DFRobot_BME280.h"
+
+typedef DFRobot_BME280_IIC    BME;    // ******** use abbreviations instead of full names ********
+
+/**IIC address is 0x77 when pin SDO is high */
+/**IIC address is 0x76 when pin SDO is low */
+BME   bme(&Wire, 0x77);   // select TwoWire peripheral and set sensor address
+
+#define SEA_LEVEL_PRESSURE    1015.0f
+
+// show last sensor operate status
+void printLastOperateStatus(BME::eStatus_t eStatus)
+{
+  switch(eStatus) {
+  case BME::eStatusOK:    Serial.println("everything ok"); break;
+  case BME::eStatusErr:   Serial.println("unknow error"); break;
+  case BME::eStatusErrDeviceNotDetected:    Serial.println("device not detected"); break;
+  case BME::eStatusErrParameter:    Serial.println("parameter error"); break;
+  default: Serial.println("unknow status"); break;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  bme.reset();
+  Serial.println("bme read data test");
+  while(bme.begin() != BME::eStatusOK) {
+    Serial.println("bme begin faild");
+    printLastOperateStatus(bme.lastOperateStatus);
+    delay(2000);
+  }
+  Serial.println("bme begin success");
+  delay(100);
+}
+
+void loop()
+{
+  float   temp = bme.getTemperature();
+  uint32_t    press = bme.getPressure();
+  float   alti = bme.calAltitude(SEA_LEVEL_PRESSURE, press);
+  float   humi = bme.getHumidity();
+
+  Serial.println();
+  Serial.println("======== start print ========");
+  Serial.print("temperature (unit Celsius): "); Serial.println(temp);
+  Serial.print("pressure (unit pa):         "); Serial.println(press);
+  Serial.print("altitude (unit meter):      "); Serial.println(alti);
+  Serial.print("humidity (unit percent):    "); Serial.println(humi);
+  Serial.println("========  end print  ========");
+
+  delay(1000);
+}
---
+++ ZG_Particle_Firmware/src/ZG_Particle_Firmware.cpp
@@ -0,0 +1,80 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/bertan/Developer/Zafer-Greenhouse/ZG_Particle_Firmware/src/ZG_Particle_Firmware.ino"
+/*
+ * Project ZG_Particle_Firmware
+ * Description: Particle Photon Greenhouse Management and Monitoring System
+ * Author: Bertan
+ * Date: October 10 2020
+ */
+
+/*
+  The firmware includes code for all functionality to be used in the greenhouse, that is:
+  - Environmental monitoring (temperature, humidity, CO2)
+  - Soil moisture monitoring for 5 pots
+  - Watering control with a solenoid valve
+
+  As multiple Particle devices will be used in the greenhouse and each will be responsible of carrying out different tasks,
+  you should modify the code for each device by commenting out the functionality you don't need.
+
+  If anyone from my school is reading this after several years, HI THERE! :) Please don't hesitate to contact me with any questions.
+*/
+
+// Include libraries for the environmental monitoring sensor
+#include <DFRobot_ENS160.h>
+#include "DFRobot_BME280.h"
+
+// Define soil moisture sensor pins
+void setup();
+void loop();
+#line 25 "/Users/bertan/Developer/Zafer-Greenhouse/ZG_Particle_Firmware/src/ZG_Particle_Firmware.ino"
+#define soil_1 A0
+#define soil_2 A1
+#define soil_3 A2
+#define soil_4 A3
+#define soil_5 A4
+
+// Define solenoid water valve pin (valve will be connected to an N-type MOSFET. See schematic for details.)
+#define water_valve D6
+
+/*
+  Calibrate each soil moisture data and enter the corresponding values here
+  Check out the wiki PDF for the soil moisture sensor for more info on how to calibrate
+*/
+#define MOISTURE_SENSOR_1_DRY 0
+#define MOISTURE_SENSOR_1_WET 4095
+
+#define MOISTURE_SENSOR_2_DRY 0
+#define MOISTURE_SENSOR_2_WET 4095
+
+#define MOISTURE_SENSOR_3_DRY 0
+#define MOISTURE_SENSOR_3_WET 4095
+
+#define MOISTURE_SENSOR_4_DRY 0
+#define MOISTURE_SENSOR_4_WET 4095
+
+#define MOISTURE_SENSOR_5_DRY 0
+#define MOISTURE_SENSOR_5_WET 4095
+
+// Arduino standard setup method
+void setup() {
+  // Set soil moisture sensor pin modes
+  pinMode(soil_1, INPUT);
+  pinMode(soil_2, INPUT);
+  pinMode(soil_3, INPUT);
+  pinMode(soil_4, INPUT);
+  pinMode(soil_5, INPUT);
+
+  // Set solenoid water valve pin mode
+  pinMode(water_valve, OUTPUT);
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
