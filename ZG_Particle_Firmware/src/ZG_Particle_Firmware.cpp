eb6ea6f255c86040e501e1ceedfac186e7820567
M. Bertan Tarakçıoğlu <83073748+BertanT@users.noreply.github.com>
2022-10-08 16:45:01 +0300

    Move to cloud events from cloud variables

--- ZG_Particle_Firmware/src/ZG_Particle_Firmware.cpp
+++ ZG_Particle_Firmware/src/ZG_Particle_Firmware.cpp
@@ -3,15 +3,13 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "/Users/bertan/Developer/Zafer-Greenhouse/ZG_Particle_Firmware/src/ZG_Particle_Firmware.ino"
+#line 1 "/Users/bertan/Developer/GitRepositories/Zafer-Greenhouse/ZG_Particle_Firmware/src/ZG_Particle_Firmware.ino"
 /*
  * Project ZG_Particle_Firmware
  * Description: Particle Photon Greenhouse Management and Monitoring System
- * Author: Bertan
+ * Author: Mehmet Bertan Tarakçıoğlu
  * Date: October 10 2020
- */
 
-/*
   The firmware includes code for all functionality to be used in the greenhouse, that is:
   - Environmental monitoring (temperature, humidity, CO2)
   - Soil moisture monitoring for 5 pots
@@ -21,16 +19,43 @@
   you should modify the code for each device by commenting out the functionality you don't need.
 
   If anyone from my school is reading this after several years, HI THERE! :) Please don't hesitate to contact me with any questions.
+
+  Copyright (c) 2022 M. Bertan Tarakçıoğlu
 */
 
 // Include libraries for the environmental monitoring sensor
 #include <DFRobot_ENS160.h>
 #include "DFRobot_BME280.h"
 
-// Define soil moisture sensor pins
+// Time between measurements in seconds
+inline void softDelay(uint32_t duration);
+int read_soil_rh(int soil_pin, int soil_dry, int soil_wet);
+int setWatering(String is_on);
 void setup();
+int updateReadings();
 void loop();
-#line 25 "/Users/bertan/Developer/Zafer-Greenhouse/ZG_Particle_Firmware/src/ZG_Particle_Firmware.ino"
+#line 25 "/Users/bertan/Developer/GitRepositories/Zafer-Greenhouse/ZG_Particle_Firmware/src/ZG_Particle_Firmware.ino"
+#define MEASUREMENT_INTERVAL 300
+
+// Define the sea level pressure in the current location for BME280 calibration
+#define SEA_LEVEL_PRESSURE 1013.25
+
+/*
+  Calibrate each soil moisture data and enter the corresponding values here
+  Check out the wiki PDF for the soil moisture sensor for more info on how to calibrate
+*/
+#define SOIL_1_DRY 0
+#define SOIL_1_WET 4095
+#define SOIL_2_DRY 0
+#define SOIL_2_WET 4095
+#define SOIL_3_DRY 0
+#define SOIL_3_WET 4095
+#define SOIL_4_DRY 0
+#define SOIL_4_WET 4095
+#define SOIL_5_DRY 0
+#define SOIL_5_WET 4095
+
+// Define soil moisture sensor pins
 #define soil_1 A0
 #define soil_2 A1
 #define soil_3 A2
@@ -40,27 +65,64 @@ void loop();
 // Define solenoid water valve pin (valve will be connected to an N-type MOSFET. See schematic for details.)
 #define water_valve D6
 
-/*
-  Calibrate each soil moisture data and enter the corresponding values here
-  Check out the wiki PDF for the soil moisture sensor for more info on how to calibrate
-*/
-#define MOISTURE_SENSOR_1_DRY 0
-#define MOISTURE_SENSOR_1_WET 4095
+// Define environmental monitoring sensor
+DFRobot_BME280_IIC bme280(&Wire, 0x76);
+DFRobot_ENS160_I2C ens160(&Wire, 0x53);
 
-#define MOISTURE_SENSOR_2_DRY 0
-#define MOISTURE_SENSOR_2_WET 4095
+// Define soft delay function!
+// The normal delay prevents Particle.process() from being called automatically, which is necessary for maintaining cloud connection.
+inline void softDelay(uint32_t duration) {
+    for (uint32_t ms = millis(); millis() - ms < duration; Particle.process());
+}
 
-#define MOISTURE_SENSOR_3_DRY 0
-#define MOISTURE_SENSOR_3_WET 4095
+// Define variables for the soil moisture sensors
+double soil_1_rh;
+double soil_2_rh;
+double soil_3_rh;
+double soil_4_rh;
+double soil_5_rh;
 
-#define MOISTURE_SENSOR_4_DRY 0
-#define MOISTURE_SENSOR_4_WET 4095
+// Define variable to track watering system state
+bool is_watering = false;
 
-#define MOISTURE_SENSOR_5_DRY 0
-#define MOISTURE_SENSOR_5_WET 4095
+// Define variables for the environmental monitoring sensor
+double temperature;
+double humidity;
+double barometric_pressure;
+double eCO2;
+double TVOC;
+double AQI;
+
+// Function to read relative humidity from soil moisture sensor
+int read_soil_rh(int soil_pin, int soil_dry, int soil_wet) {
+    int soil_value = analogRead(soil_pin);
+    int soil_rh = map(soil_value, soil_dry, soil_wet, 0, 100);
+    return soil_rh;
+}
+
+// Function to turn the watering system on and off
+int setWatering(String is_on) {
+  if (is_on == "true") {
+    digitalWrite(water_valve, HIGH);
+    is_watering = true;
+    Serial.println("Watering system turned on!");
+  } else if (is_on == "false") {
+    digitalWrite(water_valve, LOW);
+    is_watering = false;
+    Serial.println("Watering system turned off!");
+  }else {
+    Serial.println("Invalid command for function setWatering!");
+    return -1;
+  }
+  return 0;
+}
 
 // Arduino standard setup method
 void setup() {
+  // Start serial communication
+  Serial.begin(115200);
+  Serial.println("Welcome to the Zafer Smart Greenhouse Project! Courtesy of IB Class of 23 :)");
+
   // Set soil moisture sensor pin modes
   pinMode(soil_1, INPUT);
   pinMode(soil_2, INPUT);
@@ -71,10 +133,68 @@ void setup() {
   // Set solenoid water valve pin mode
   pinMode(water_valve, OUTPUT);
 
+  // Start and configure BME280 sensor
+  if (!bme280.begin()) {
+    Serial.println("BME280 initialization failed! Will restart in 30 seconds");
+    delay(30000);
+    System.reset();
+  } else {
+    Serial.println("BME280 initialization successful!");
+  }
+
+  // Start and configure ENS160 sensor
+  if (!ens160.begin()) {
+    Serial.println("ENS160 initialization failed! Will restart in 30 seconds");
+    delay(30000);
+    System.reset();
+  } else {
+    Serial.println("ENS160 initialization successful!");
+  }
+  ens160.setPWRMode(ENS160_STANDARD_MODE);
+
+  // * Cloud Setup! *
+  // Register soil moisture variables to the cloud
+  Particle.variable("soil_1_rh", soil_1_rh);
+  Particle.variable("soil_2_rh", soil_2_rh);
+  Particle.variable("soil_3_rh", soil_3_rh);
+  Particle.variable("soil_4_rh", soil_4_rh);
+  Particle.variable("soil_5_rh", soil_5_rh);
+
+  // Register watering control function to the cloud
+  Particle.function("setWatering", setWatering);
+  // Register watering status function as a calculated variable to the cloud
+  Particle.variable("isWatering", is_watering);
+
+  // Register environmental monitoring variables to the cloud
+  Particle.variable("temperature", temperature);
+  Particle.variable("humidity", humidity);
+  Particle.variable("barometric_pressure", barometric_pressure);
+  Particle.variable("eCO2", eCO2);
+  Particle.variable("TVOC", TVOC);
+  Particle.variable("AQI", AQI);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+// Unified function to update all sensor data
+int updateReadings() {
+  // Update soil moisture sensor readings
+  soil_1_rh = read_soil_rh(soil_1, SOIL_1_DRY, SOIL_1_WET);
+  soil_2_rh = read_soil_rh(soil_2, SOIL_2_DRY, SOIL_2_WET);
+  soil_3_rh = read_soil_rh(soil_3, SOIL_3_DRY, SOIL_3_WET);
+  soil_4_rh = read_soil_rh(soil_4, SOIL_4_DRY, SOIL_4_WET);
+  soil_5_rh = read_soil_rh(soil_5, SOIL_5_DRY, SOIL_5_WET);
 
+  // Update environmental monitoring sensor readings
+  temperature = bme280.getTemperature();
+  humidity = bme280.getHumidity();
+  barometric_pressure = bme280.getPressure();
+  eCO2 = ens160.getECO2();
+  TVOC = ens160.getTVOC();
+  AQI = ens160.getAQI();
+  return 0;
+}
+
+void loop() {
+  // Periodically calibrate the ENS160 sensor according to the current conditions
+  ens160.setTempAndHum(bme280.getTemperature(), bme280.getHumidity());
+  softDelay(MEASUREMENT_INTERVAL*1000);
 }
